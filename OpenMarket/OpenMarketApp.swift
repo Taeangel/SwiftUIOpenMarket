3d56b4832f7e50c9f2ab3a0dc497a8f86c7708c5
Taeangel <wngks2050@gmail.com>
2022-10-20 10:04:11 +0900

    Initial Commit

---
+++ OpenMarket/OpenMarketApp.swift
@@ -0,0 +1,17 @@
+//
+//  OpenMarketApp.swift
+//  OpenMarket
+//
+//  Created by song on 2022/10/20.
+//
+
+import SwiftUI
+
+@main
+struct OpenMarketApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
